6eba642af197df36855dc90eebe3462c55df773c
RashadAbdullah <Rashad.roshdy72@gmail.com>
2026-01-18 10:31:27 +0200

    Initial commit: simple Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static String sayHello() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+}
